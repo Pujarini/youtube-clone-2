dfd923922878fe3d7058e685c7e0fd72f4a7073e
Pujarini Jena <pujarinijena@Pujarini-MacBook-Pro.local>
2023-02-02 15:15:16 +0530

    Assignment-18

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,10 +1,56 @@
+import { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchVideos } from "../constants/fetchVideos";
+import { cacheVideoList, clearVideos, filterVideos } from "../redux/videoSlice";
+
 const SearchBar = () => {
+  const [search, setSearch] = useState("");
+
+  const dispatch = useDispatch();
+
+  const videos = useSelector((store) => store.videoList.videos);
+
+  const searchHandler = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const fetchVideoList = async () => {
+    const data = await fetchVideos();
+    dispatch(cacheVideoList(data));
+  };
+
+  const searchVideos = (keyword) => {
+    let filteredVideos = [];
+    if (keyword.length) {
+      filteredVideos = videos.filter((video) =>
+        video?.snippet?.title.toLowerCase().includes(keyword)
+      );
+    }
+    if (filteredVideos.length) {
+      dispatch(filterVideos(filteredVideos));
+    } else {
+      dispatch(clearVideos());
+    }
+
+    if (!keyword) {
+      fetchVideoList();
+    }
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => searchVideos(search), 1000);
+    return () => clearTimeout(timer);
+    // eslint-disable-next-line
+  }, [search]);
+
   return (
     <div>
       <input
         type="text"
         placeholder="search"
         className="w-80 outline-none rounded-full h-[30px] p-5 border-slate-400 border"
+        value={search}
+        onChange={searchHandler}
       />
     </div>
   );
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,24 +1,35 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchVideos } from "../constants/fetchVideos";
+import { cacheVideoList } from "../redux/videoSlice";
 import VideoCard from "./VideoCard";
 
 const VideoList = () => {
-  const [videos, setVideos] = useState([]);
+  const videos = useSelector((store) => store.videoList.videos);
+
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    fetchVideoList();
+    if (!videos.length) {
+      fetchVideoList();
+    }
+    // eslint-disable-next-line
   }, []);
 
   const fetchVideoList = async () => {
     const data = await fetchVideos();
-    setVideos(data);
+    dispatch(cacheVideoList(data));
   };
 
   return (
     <div className="flex gap-5 flex-wrap w-full">
-      {videos &&
+      {!videos.length ? (
+        <div>No videos found </div>
+      ) : (
         videos.map((video) => {
           return <VideoCard video={video} key={video.id} />;
-        })}
+        })
+      )}
     </div>
   );
 };
